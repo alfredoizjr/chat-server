8bd9fa4ef46bbec7797920cf9612a413e18421b9
Alfredo <alfredo.alucar@gmail.com>
2018-05-25 01:08:31 -0400

    add desconnect

--- index.js
+++ index.js
@@ -10,14 +10,19 @@ io.on('connection', function(socket){
 
     socket.on('set-nickname', (nickname) => {
         socket.nickname = nickname;
-        io.emit('users-changed', {user: nickname, event: 'joined'});    
+        io.emit('users-changed', {user: nickname, event: 'login'});    
       });
 
     socket.on('add-message', (message) => {
         io.emit('message', {text: message.text, from: socket.nickname, created: new Date()});    
       });
 
+      socket.on('disconnect', function(){
+        io.emit('users-changed', {user: socket.nickname, event: 'logout'});   
+      });
+
     });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
